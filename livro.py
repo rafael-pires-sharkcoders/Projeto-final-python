6ddffeb9c94fe39a147b2c3b42bd2ed29ef709e1
rafael-pires-sharkcoders <149233319+rafael-pires-sharkcoders@users.noreply.github.com>
2024-06-19 16:47:30 +0100

    livro.py

---
+++ livro.py
@@ -0,0 +1,7 @@
+class Livros:
+    def __init__(self, titulo, autor, preco, genero, quantidade_de_paginas):
+        self.titulo = titulo
+        self.autor = autor
+        self.preco = preco
+        self.genero = genero
+        self.quantidade_de_paginas = quantidade_de_paginas
